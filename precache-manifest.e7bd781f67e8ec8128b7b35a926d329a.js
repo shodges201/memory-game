34fda344eff0725ea531ceace721124b7be0c1c0
Scott Hodges <shodges201@gmail.com>
2019-08-27 18:40:03 -0400

    Updates

---
+++ precache-manifest.e7bd781f67e8ec8128b7b35a926d329a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6b0e4d79422602ccea0b2c89aed9db",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "abf8343fa44ba3583804",
+    "url": "/memory-game/static/css/main.459e28b2.chunk.css"
+  },
+  {
+    "revision": "3dd8da646ece53bb9fca",
+    "url": "/memory-game/static/js/2.3f4bbd8f.chunk.js"
+  },
+  {
+    "revision": "abf8343fa44ba3583804",
+    "url": "/memory-game/static/js/main.073f18d7.chunk.js"
+  },
+  {
+    "revision": "0f7388fea29552719e48",
+    "url": "/memory-game/static/js/runtime~main.c5fd0515.js"
+  }
+]);
